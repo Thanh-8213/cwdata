1149add1821e68d6d46792a05439d200e85f63ad
Thomas Nguyen <dngu0079@student.monash.edu>
2020-10-19 14:47:38 +1100

    Change in README file

---
+++ README.md
@@ -0,0 +1,24 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# cwdata
+
+<!-- badges: start -->
+
+<!-- badges: end -->
+
+The goal of cwdata is to use in ETC5523
+
+## Installation
+
+You can install the released version of cwdata from
+[CRAN](https://CRAN.R-project.org) with:
+
+``` r
+install.packages("cwdata")
+```
+
+And the development version from [GitHub](https://github.com/) with:
+
+\`\`\` r \# install.packages(“devtools”)
+devtools::install\_github(“Thanh-8213/cwdata”)
---
+++ data-raw/key_crop_yields.R
@@ -0,0 +1,22 @@
+## code to prepare `key_crop_yields` dataset goes here
+
+library(tidyverse)
+library(readr)
+library(janitor)
+library(tidyr)
+library(magrittr)
+url <- 'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-09-01/key_crop_yields.csv'
+
+key_crop_yields <- read_csv(url) %>%
+  clean_names() %>%
+  pivot_longer(
+    cols = c(-entity, - code, - year),
+    names_to = "crop",
+    values_to = "tonnes_per_hectare",
+    names_pattern = "([^_]+)"
+    # The names pattern is a regular expression, meaning that take everything
+    # that is before the the first under-score
+  ) %>%
+  rename(country = entity)
+
+usethis::use_data(key_crop_yields, overwrite = TRUE)
